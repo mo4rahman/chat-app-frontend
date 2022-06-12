4e3684f890a52ba6dc5fcf3fa3bb005676ea48a8
mo4rahman <mo4rahman@gmail.com>
2022-06-12 17:13:52 -0400

    styling to register/sign-in page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+
+function handleChange(event) {
+  // event.preventDefault();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("form");
+}
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <FormContainter>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="Logo" />
+            <h1>chatty</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="Password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Enter password again"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainter>
+    </>
+  );
 }
 
-export default Register
+const FormContainter = styled.div`
+  height: 100vg;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #03bf83;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+  }
+`;
+
+export default Register;
