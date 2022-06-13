8c4a89798a1c150c7dff483ec24401435c8b4b39
mo4rahman <mo4rahman@gmail.com>
2022-06-13 02:24:07 -0400

    completed profile pic/avatar page

--- src/pages/SetAvatar.jsx
+++ src/pages/SetAvatar.jsx
@@ -26,7 +26,38 @@ function SetAvatar() {
     draggable: true,
   };
 
-  //   async function setProfilePicture(){};
+  useEffect(() => {
+    async function fetchData() {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+      }
+    }
+    fetchData();
+  }, []);
+
+  async function setProfilePicture() {
+    if (selectedAvatar === undefined) {
+      toast.error("Please select an avatar", toastOptions);
+    } else {
+      const user = await JSON.parse(localStorage.getItem("chat-app-user"));
+      console.log(user);
+      // data destructured from the axios api
+      const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
+        image: avatars[selectedAvatar],
+      });
+      if (data.isSet) {
+        user.isAvatarImageSet = true;
+        user.avatarImage = data.image;
+        localStorage.setItem("chat-app-user", JSON.stringify(user));
+        navigate("/");
+      } else {
+        toast.error(
+          "Having toruble setting the avatar. Please try again in a bit.",
+          toastOptions
+        );
+      }
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -46,34 +77,94 @@ function SetAvatar() {
 
   return (
     <>
-      <Container>
-        <div className="title-container">
-          <h1>Please pick an avaar to set as your profile icon!</h1>
-        </div>
-        <div className="avatars">
-          {avatars.map((avatar, index) => {
-            return (
-              <div
-                key={index}
-                className={`avatar ${
-                  selectedAvatar === index ? "selected" : ""
-                }`}
-              >
-                <img
-                  src={`data:image/svg+xml;base64,${avatar}`}
-                  alt="avatar"
-                  onClick={() => setSelectedAvatar(index)}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </Container>
+      {isLoading ? (
+        <Container>
+          <img src={Loading} alt="loading" className="loading" />
+        </Container>
+      ) : (
+        <Container>
+          <div className="title-container">
+            <h1>Please pick an avatar to set as your profile icon!</h1>
+          </div>
+          <div className="avatars">
+            {avatars.map((avatar, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`avatar ${
+                    selectedAvatar === index ? "selected" : ""
+                  }`}
+                >
+                  <img
+                    src={`data:image/svg+xml;base64,${avatar}`}
+                    alt="avatar"
+                    onClick={() => setSelectedAvatar(index)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <button className="submit-btn" onClick={setProfilePicture}>
+            Set as your profile avatar
+          </button>
+        </Container>
+      )}
+
       <ToastContainer />
     </>
   );
 }
 
-const Container = styled.div``;
-
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #02bd82;
+  height: 100vh;
+  width: 100vw;
+  gap: 2rem;
+  .loading {
+    max-inline-size: 100%;
+  }
+  .title-container {
+    h1 {
+      color: white;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid yellow;
+    }
+  }
+  .submit-btn {
+    background-color: blue;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    transition: 0.5s ease-in-out;
+    &:hover {
+      background-color: #230672;
+    }
+  }
+`;
 export default SetAvatar;
