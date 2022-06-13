ebf2e6acb8ceaf5e1043c98ab7f9634892d3ec17
mo4rahman <mo4rahman@gmail.com>
2022-06-13 10:15:45 -0400

    test

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,6 @@
-export const host = "http://localhost:4000";
+// export const host = "http://localhost:4000";
+export const host = "https://mohammed-chat-app-backend.herokuapp.com";
+
 // Have to do it like this because you can only have one export default
 export const signupRoute = `${host}/api/auth/signup`;
 export const loginRoute = `${host}/api/auth/login`;
